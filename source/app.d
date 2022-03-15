d08555de0f2277b73d4caa9e80f0e81b6571ac4c
Ikey Doherty <ikey@serpentos.com>
2022-03-15 01:12:47 +0000

    Add skeletal vibe.d application
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/app.d
@@ -0,0 +1,21 @@
+import vibe.vibe;
+
+void main()
+{
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	auto listener = listenHTTP(settings, &hello);
+	scope (exit)
+	{
+		listener.stopListening();
+	}
+
+	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	runApplication();
+}
+
+void hello(HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.writeBody("Hello, World!");
+}
