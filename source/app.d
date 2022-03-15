0406e04fd3e3ce87b899b0888cb870e5d00622d6
Ikey Doherty <ikey@serpentos.com>
2022-03-15 01:31:50 +0000

    app: Conform to template
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/app.d
+++ source/app.d
@@ -1,3 +1,13 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Main entry point
+ *
+ * Authors: © 2020-2022 Serpent OS Developers
+ * License: ZLib
+ */
+module main;
+
 import vibe.vibe;
 
 void main()
