bfad8e876d62fb5455bf711d7203174a75369c37
Ikey Doherty <ikey@serpentos.com>
2022-06-29 12:23:06 +0100

    app: Strip app back to nothing for reboot
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/app.d
+++ source/app.d
@@ -1,46 +1,20 @@
-/* SPDX-License-Identifier: Zlib */
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
 
 /**
- * Main entry point
+ * main
  *
- * Authors: © 2020-2022 Serpent OS Developers
- * License: ZLib
+ * Main entry point into summit app
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
  */
 
 module main;
 
-import vibe.vibe;
-
-/**
- * Simple entry point for now to help define styling
- */
-final class SummitApp
-{
-    /**
-     * Provide the main index page.
-     */
-    void index(HTTPServerRequest req, HTTPServerResponse res)
-    {
-        return res.render!"index.dt";
-    }
-}
-
-/**
- * Main executable entry, bootstrap the server
- */
 void main()
 {
-    auto settings = new HTTPServerSettings;
-    settings.port = 8080;
-    settings.bindAddresses = ["::1", "127.0.0.1"];
-    auto router = new URLRouter();
-    router.registerWebInterface(new SummitApp());
-    auto listener = listenHTTP(settings, router);
-    scope (exit)
-    {
-        listener.stopListening();
-    }
-
-    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-    runApplication();
 }
