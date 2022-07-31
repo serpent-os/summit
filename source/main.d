0d4e635192a3ff851af0e1a9034f9486f5df8532
Ikey Doherty <ikey@serpentos.com>
2022-07-31 13:06:34 +0100

    main: Cleanup
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/main.d
+++ source/main.d
@@ -19,11 +19,24 @@ import vibe.d;
 import libsodium;
 import summit.app;
 
+/**
+ * Gets our dashboard web + RPC up and running
+ *
+ * Throws: Exception if libsodium cannot be initialised
+ * Params:
+ *      args = CLI arguments
+ * Returns: 0 if everything went to plan
+ */
 int main(string[] args)
 {
+    /**
+     * Get sodium setup
+     */
+    logInfo("Initialising libsodium");
     const ret = sodium_init();
     enforce(ret == 0, "Failed to initialise sodium");
 
+    logInfo("Starting Summit");
     auto app = new SummitApp();
     app.start();
     scope (exit)
