1d170ad4fda50eea6ff99938cad8285fc39cdff8
Ikey Doherty <ikey@serpentos.com>
2022-07-31 12:56:40 +0100

    web/accounts: Create our initial account paths
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/web/accounts.d
+++ source/summit/web/accounts.d
@@ -38,4 +38,32 @@ import vibe.d;
     {
         render!"accounts/login.dt";
     }
+
+    /**
+     * Attempt to perform the actual login
+     * This will be dispatched via the AccountsManager
+     *
+     * Params:
+     *      username = Account ID to login with
+     *      password = The account plaintext password.
+     */
+    @path("login") @method(HTTPMethod.POST) void loginUser(ValidUsername username,
+            ValidPassword password) @safe
+    {
+    }
+
+    /**
+     * Attempt registration of the user
+     * We rely on client-side form validation and require valid username, password + email address.
+     *
+     * Params:
+     *      username = New username
+     *      password = New password
+     *      confirmPassword = Ensure passwords match
+     *      email = Users email address (validation)
+     */
+    @path("register") @method(HTTPMethod.POST) void registerUser(ValidUsername username,
+            ValidPassword password, Confirm!"password" confirmPassword, ValidEmail email) @safe
+    {
+    }
 }
