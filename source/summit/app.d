f03dfa03c9c7e6d495146e75f2cb2cea2e3f7954
Ikey Doherty <ikey@serpentos.com>
2022-10-11 11:38:30 +0100

    app: Set up lifecycle for server, enable session storage (moss-db)
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/app.d
+++ source/summit/app.d
@@ -16,6 +16,9 @@
 module summit.app;
 
 import vibe.d;
+import moss.service.sessionstore;
+import std.file : mkdirRecurse;
+import std.path : buildPath;
 
 /**
  * SummitApplication maintains the core lifecycle of Summit
@@ -34,6 +37,28 @@ public final class SummitApplication
     this(string rootDir) @safe
     {
         logInfo(format!"SummitApplication running from %s"(rootDir));
+
+        immutable dbPath = rootDir.buildPath("db");
+        dbPath.mkdirRecurse();
+
+        router = new URLRouter();
+
+        /* Set up the server */
+        serverSettings = new HTTPServerSettings();
+        serverSettings.bindAddresses = ["localhost",];
+        serverSettings.disableDistHost = true;
+        serverSettings.useCompressionIfPossible = true;
+        serverSettings.port = 8081;
+        serverSettings.sessionOptions = SessionOption.secure | SessionOption.httpOnly;
+        serverSettings.serverString = "summit/0.0.1";
+        serverSettings.sessionIdCookie = "summit.session_id";
+
+        /* Session persistence */
+        sessionStore = new DBSessionStore(dbPath.buildPath("session"));
+        serverSettings.sessionStore = sessionStore;
+
+        /* Lets go listen */
+        listener = listenHTTP(serverSettings, router);
     }
 
     /**
@@ -41,6 +66,13 @@ public final class SummitApplication
      */
     void close() @safe
     {
-
+        listener.stopListening();
     }
+
+private:
+
+    HTTPListener listener;
+    HTTPServerSettings serverSettings;
+    URLRouter router;
+    DBSessionStore sessionStore;
 }
