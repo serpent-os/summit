bad73a269f2027e18c52b1b91e2c98bf8b942e5d
Ikey Doherty <ikey@serpentos.com>
2022-10-11 21:17:59 +0100

    web: Begin the Collections visual module
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/web/collections.d
+++ source/summit/web/collections.d
@@ -5,43 +5,40 @@
  */
 
 /**
- * summit.web
+ * summit.web.collections
  *
- * Root web application (nested)
+ * Collections Web API
  *
  * Authors: Copyright © 2020-2022 Serpent OS Developers
  * License: Zlib
  */
 
-module summit.web;
+module summit.web.collections;
 
 import vibe.d;
-import summit.web.collections;
 
 /**
  * Root entry into our web service
  */
-@path("/")
-public final class SummitWeb
+@path("/collections")
+public final class CollectionsWeb
 {
     /**
-     * Join SummitWeb into the router
+     * Join CollectionsWeb into the router
      *
      * Params:
-     *      router = Base root for the application
+     *      router = Web root for the application
      */
     @noRoute void configure(URLRouter router) @safe
     {
-        auto root = registerWebInterface(router, this);
-        auto col = new CollectionsWeb();
-        col.configure(root);
+        registerWebInterface(router, this);
     }
 
     /**
-     * Return the "home" page
+     * Collections index
      */
     void index() @safe
     {
-        render!"index.dt";
+        render!"collections/index.dt";
     }
 }
