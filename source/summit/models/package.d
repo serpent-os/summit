c92935b2c4c7e83c1842ee90c0f9253e473d4282
Ikey Doherty <ikey@serpentos.com>
2022-10-12 21:21:41 +0100

    models: Stub a new models package
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/summit/models/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.models
+ *
+ * Models within summit
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.models;
