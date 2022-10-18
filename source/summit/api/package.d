57d24db0b4233ce0b457edf7719b3cbb3f4e860b
Ikey Doherty <ikey@serpentos.com>
2022-10-18 11:35:26 +0100

    api: Start the proper structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/summit/api/package.d
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.api
+ *
+ * Core API for Summit
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.api;
+
+/**
+ * Current primary version
+ */
+public import summit.api.v1;
---
+++ source/summit/api/v1/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.api.v1
+ *
+ * V1 Summit API
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.api.v1;
