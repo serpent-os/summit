408fba60e2449b7f38635792ed02431ed7e61185
Ikey Doherty <ikey@serpentos.com>
2022-11-19 14:59:25 +0000

    setup: Start establishing the setup app structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/summit/setup.d
@@ -0,0 +1,54 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.setup
+ *
+ * Web-based setup application
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.setup;
+
+import vibe.d;
+
+/**
+ * SetupApplication is only constructed when we actually
+ * need first run configuration
+ */
+public final class SetupApplication
+{
+    /**
+     * Construct a new SetupApplication
+     */
+    this() @safe
+    {
+        _router = new URLRouter();
+        _router.registerWebInterface(this);
+    }
+
+    /**
+     * Render the index page
+     */
+    void index() @safe
+    {
+        render!"setup/index.dt";
+    }
+
+    /**
+     * Access the underlying URLRouter
+     */
+    @noRoute pragma(inline, true) pure @property URLRouter router() @safe @nogc nothrow
+    {
+        return _router;
+    }
+
+private:
+
+    URLRouter _router;
+}
