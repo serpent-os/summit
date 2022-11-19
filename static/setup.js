db884937bbe9ae0e1f8bd3c1bb4ee40a0beaa91d
Ikey Doherty <ikey@serpentos.com>
2022-11-19 19:40:40 +0000

    setup.js: Add missing doc header
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- static/setup.js
+++ static/setup.js
@@ -1,3 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * setup.js
+ *
+ * Helpers for setup mode
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
 window.addEventListener('load', function(ev)
 {
     const form = document.getElementById('setupForm');
