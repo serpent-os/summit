23ac386542a73802a8b643668168c7d817cf8b90
Ikey Doherty <ikey@serpentos.com>
2022-11-22 23:18:39 +0000

    collections/repository: Flesh out the basic design/structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/collections/repository.d
+++ source/summit/collections/repository.d
@@ -15,10 +15,57 @@
 
 module summit.collections.repository;
 
+import moss.client.metadb;
+import summit.collections.collection;
+import summit.context;
+import summit.models.repository;
+
 /**
  * An explicitly managed repository
+ *
+ * Note in the design for Summit we opted for monorepos and minimal collections.
+ * We don't intend to support thousands of parallel DB connections, and the community
+ * collection handles all the "personal use" cases quite nicely.
+ *
+ * Thus the decision to rely on MetaDB/LMDB is ok at this minimalist scale.
+ *
  */
 public final class ManagedRepository
 {
+    @disable this();
 
+    /**
+     * Construct a new ManagedRepository from an input model
+     *
+     * Params:
+     *      context = global context
+     *      parent = Parent collection
+     *      model = Database model
+     */
+    this(SummitContext context, ManagedCollection parent, Repository model) @safe
+    {
+        this._model = model;
+    }
+
+    /**
+     * Returns: database connection
+     */
+    pure @property MetaDB db() @safe @nogc nothrow
+    {
+        return _db;
+    }
+
+    /**
+     * Returns: Underlying database model
+     */
+    pure @property Repository model() @safe @nogc nothrow
+    {
+        return _model;
+    }
+
+private:
+
+    SummitContext context;
+    MetaDB _db;
+    Repository _model;
 }
