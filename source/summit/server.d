c2abfe91a51d91e807eaa067417181bb0a943159
Ikey Doherty <ikey@serpentos.com>
2023-01-01 12:21:19 +0000

    Update copyright ranges
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/server.d
+++ source/summit/server.d
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
  *
  * SPDX-License-Identifier: Zlib
  */
@@ -10,7 +10,7 @@
  * Primary server process. Enables switching between setup and primary
  * application, only enables the session store by itself
  *
- * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
  * License: Zlib
  */
 
