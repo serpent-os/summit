70e61682283f00feac1b807a0a9b70e7d40834c6
Ikey Doherty <ikey@serpentos.com>
2023-01-13 21:40:12 +0000

    fixtures: Split fixture handling from the main app
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/app.d
+++ source/summit/app.d
@@ -22,12 +22,11 @@ import moss.service.models;
 import std.path : buildPath;
 import summit.api;
 import summit.build;
+import summit.fixtures;
 import summit.models;
 import summit.projects;
 import summit.web;
-import vibe.core.file : readFileUTF8;
 import vibe.d;
-import vibe.data.json;
 
 /**
  * SummitApplication provides the main dashboard application
@@ -54,7 +53,7 @@ public final class SummitApplication
         web = new SummitWeb(context, projectManager, router);
         service = new RESTService(context, projectManager, router);
 
-        loadFixtures();
+        loadFixtures(context, projectManager);
     }
 
     /**
@@ -74,100 +73,6 @@ public final class SummitApplication
     }
 
 private:
-
-    static struct FixtureRepo
-    {
-        string name;
-        string summary;
-        string uri;
-    }
-
-    static struct FixtureRemote
-    {
-        string name;
-        uint priority;
-        string uri;
-    }
-
-    static struct FixtureProfile
-    {
-        string name;
-        string arch;
-        string indexURI;
-        FixtureRemote[] remotes;
-    }
-
-    static struct FixtureProject
-    {
-        string name;
-        string slug;
-        string description;
-        @optional FixtureProfile[] profiles;
-        @optional FixtureRepo[] repos;
-    }
-
-    static struct Fixture
-    {
-        FixtureProject[] projects;
-    }
-
-    void loadFixtures() @safe
-    {
-        immutable fixturePath = context.rootDirectory.buildPath("seed.json");
-        auto fixture = fixturePath.readFileUTF8;
-        auto fixtureRoot = parseJson(fixture);
-        Fixture config = deserialize!(JsonSerializer, Fixture)(fixtureRoot);
-
-        foreach (proj; config.projects)
-        {
-            auto project = projectManager.bySlug(proj.slug);
-            /* Construct missing data for this project */
-            if (project is null)
-            {
-                Project pj;
-                pj.name = proj.name;
-                pj.slug = proj.slug;
-                pj.summary = proj.description;
-                immutable err = projectManager.addProject(pj);
-                enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
-                project = projectManager.bySlug(proj.slug);
-            }
-
-            /* Ensure the repos are added */
-            foreach (repo; proj.repos)
-            {
-                auto l = project.bySlug(repo.name);
-                if (l !is null)
-                {
-                    continue;
-                }
-                Repository r;
-                r.name = repo.name;
-                r.summary = repo.summary;
-                r.originURI = repo.uri;
-                immutable err = project.addRepository(r);
-                enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
-            }
-
-            /* Load the profiles */
-            foreach (profile; proj.profiles)
-            {
-                auto l = project.profile(profile.name);
-                if (l !is null)
-                {
-                    continue;
-                }
-
-                /* Construct it.. */
-                Profile p;
-                p.name = profile.name;
-                p.arch = profile.arch;
-                p.volatileIndexURI = profile.indexURI;
-                immutable err = project.addProfile(p);
-                enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
-            }
-        }
-    }
 
     ProjectManager projectManager;
     BuildManager buildManager;
---
+++ source/summit/fixtures.d
@@ -0,0 +1,120 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.fixtures
+ *
+ * Basic JSON fixture mapping
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.fixtures;
+
+import moss.service.context;
+import std.path : buildPath;
+import summit.models;
+import summit.projects;
+import vibe.core.file : readFileUTF8;
+import vibe.d;
+import vibe.data.json;
+
+package:
+
+struct FixtureRepo
+{
+    string name;
+    string summary;
+    string uri;
+}
+
+struct FixtureRemote
+{
+    string name;
+    uint priority;
+    string uri;
+}
+
+struct FixtureProfile
+{
+    string name;
+    string arch;
+    string indexURI;
+    FixtureRemote[] remotes;
+}
+
+struct FixtureProject
+{
+    string name;
+    string slug;
+    string description;
+    @optional FixtureProfile[] profiles;
+    @optional FixtureRepo[] repos;
+}
+
+struct Fixture
+{
+    FixtureProject[] projects;
+}
+
+static void loadFixtures(ServiceContext context, ProjectManager projectManager) @safe
+{
+    immutable fixturePath = context.rootDirectory.buildPath("seed.json");
+    auto fixture = fixturePath.readFileUTF8;
+    auto fixtureRoot = parseJson(fixture);
+    Fixture config = deserialize!(JsonSerializer, Fixture)(fixtureRoot);
+
+    foreach (proj; config.projects)
+    {
+        auto project = projectManager.bySlug(proj.slug);
+        /* Construct missing data for this project */
+        if (project is null)
+        {
+            Project pj;
+            pj.name = proj.name;
+            pj.slug = proj.slug;
+            pj.summary = proj.description;
+            immutable err = projectManager.addProject(pj);
+            enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
+            project = projectManager.bySlug(proj.slug);
+        }
+
+        /* Ensure the repos are added */
+        foreach (repo; proj.repos)
+        {
+            auto l = project.bySlug(repo.name);
+            if (l !is null)
+            {
+                continue;
+            }
+            Repository r;
+            r.name = repo.name;
+            r.summary = repo.summary;
+            r.originURI = repo.uri;
+            immutable err = project.addRepository(r);
+            enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
+        }
+
+        /* Load the profiles */
+        foreach (profile; proj.profiles)
+        {
+            auto l = project.profile(profile.name);
+            if (l !is null)
+            {
+                continue;
+            }
+
+            /* Construct it.. */
+            Profile p;
+            p.name = profile.name;
+            p.arch = profile.arch;
+            p.volatileIndexURI = profile.indexURI;
+            immutable err = project.addProfile(p);
+            enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
+        }
+    }
+}
