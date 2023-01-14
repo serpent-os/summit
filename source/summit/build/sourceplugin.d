c85cfabf3ee9490c229cebaf8c2ba0a4f6646b1c
Ikey Doherty <ikey@serpentos.com>
2023-01-14 02:36:18 +0000

    build: Start on new source plugin API
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/summit/build/sourceplugin.d
@@ -0,0 +1,85 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * summit.build.sourceplugin
+ *
+ * Source API interface to moss-deps
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+
+module summit.build.sourceplugin;
+
+import moss.deps.registry;
+import moss.client.metadb;
+
+/**
+ * Provides an interface between our source repository definitions
+ * and the moss-deps transaction APIs
+ */
+public final class SourcePlugin : RegistryPlugin
+{
+    @disable this();
+
+    /**
+     * Construct a new SourcePlugin
+     */
+    this(MetaDB db) @safe
+    {
+        this.db = db;
+    }
+
+    override RegistryItem[] queryProviders(in ProviderType type, in string matcher,
+            ItemFlags flags = ItemFlags.None)
+    {
+        return null;
+    }
+
+    override ItemInfo info(in string pkgID) const
+    {
+        return ItemInfo.init;
+    }
+
+    override NullableRegistryItem queryID(in string pkgID)
+    {
+        return NullableRegistryItem(RegistryItem.init);
+    }
+
+    override const(Dependency)[] dependencies(in string pkgID) const
+    {
+        return null;
+    }
+
+    override const(Provider)[] providers(in string pkgID) const
+    {
+        return null;
+    }
+
+    override const(RegistryItem)[] list(in ItemFlags flags) const
+    {
+        return null;
+    }
+
+    override pure @property uint64_t priority() @safe @nogc nothrow const
+    {
+        return 0;
+    }
+
+    override Job fetchItem(in string pkgID)
+    {
+        return null;
+    }
+
+    override void close()
+    {
+    }
+
+private:
+
+    MetaDB db;
+}
