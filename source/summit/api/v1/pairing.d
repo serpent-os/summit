15c9016b7202d3dcef1e11c7f7203419e667a775
Ikey Doherty <ikey@serpentos.com>
2023-03-17 13:15:40 +0000

    api/v1/pairing: Set op builders to fully operational
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/summit/api/v1/pairing.d
+++ source/summit/api/v1/pairing.d
@@ -119,6 +119,7 @@ private:
         immutable err = context.appDB.view((in tx) => endpoint.load(tx, token.payload.sub));
         enforceHTTP(err.isNull, HTTPStatus.notFound, err.message);
         endpoint.status = EndpointStatus.Operational;
+        endpoint.statusText = "Fully operational";
         endpoint.bearerToken = request.issueToken;
         immutable errStore = context.appDB.update((scope tx) => endpoint.save(tx));
         enforceHTTP(err.isNull, HTTPStatus.internalServerError, err.message);
